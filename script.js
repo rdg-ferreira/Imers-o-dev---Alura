dbd8b2a8e0f71e24d28c93cb7fcb61c325e0e1d8
Rodrigo Ferreira <93291775+rdg-ferreira@users.noreply.github.com>
2022-03-11 20:06:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function Result () {
 
+    //nome do aluno
+    var nomeAluno = document.getElementById("nome");
+    var nome = nomeAluno.value;
+
+    // notas
     var notaDoPrimeiroBi = parseFloat(document.getElementById("1b").value);
     var notaDoSegundoBi = parseFloat(document.getElementById("2b").value);
     var notaDoTerceiroBi = parseFloat(document.getElementById("3b").value);
@@ -8,14 +13,14 @@ function Result () {
     var soma = (notaDoPrimeiroBi + notaDoSegundoBi + notaDoTerceiroBi + notaDoQuartoBi) / 4;
     
        if (soma > 7) {
-    notaFinal.innerHTML = "Você foi Aprovado(a) sua média foi "
+    notaFinal.innerHTML = "Olá, " + nome + " Você foi Aprovado(a) sua média foi "
       + 
        soma.toFixed(1)
         }
       else {
     
-     notaFinal.innerHTML =
-      "Você foi Reprovado(a) sua média foi " 
+     notaFinal.innerHTML = "Olá, " + nome + 
+      " Você foi Reprovado(a) sua média foi " 
        + soma.toFixed(1)
         }
     }
